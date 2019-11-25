efb537e25cd8abb2e1d3d91e7599741f4f18b20f
anukaw10 <anukaw10@gmail.com>
2019-11-25 18:30:26 +1300

    Commits

---
+++ generate-cards.js
@@ -0,0 +1,13 @@
+function createCard(){
+    for(var i=0; i<10; i++)
+    {
+        const card = document.createElement('div')
+        const blogger = document.createElement('h2')
+        blogger.innerHTML = "Engadget"
+        card.appendChild(blogger)
+        document.body.appendChild(card)
+    }
+
+}
+
+createCard()
